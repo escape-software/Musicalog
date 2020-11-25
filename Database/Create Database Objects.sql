c1cd705b01fc9f89faec9e35ba169822ce06815e
Kevin McGregor <kmcgregor5@gmail.com>
2020-11-25 13:28:29 +0000

    Change sql and config files.

--- Database/Create Database Objects.sql
+++ Database/Create Database Objects.sql
@@ -1,13 +1,13 @@
 USE [Musicalog]
 GO
-/****** Object:  User [MusicalogUser]    Script Date: 25/11/2020 11:27:31 ******/
+/****** Object:  User [MusicalogUser]    Script Date: 25/11/2020 12:49:35 ******/
 CREATE USER [MusicalogUser] FOR LOGIN [MusicalogUser] WITH DEFAULT_SCHEMA=[dbo]
 GO
 ALTER ROLE [db_datareader] ADD MEMBER [MusicalogUser]
 GO
 ALTER ROLE [db_datawriter] ADD MEMBER [MusicalogUser]
 GO
-/****** Object:  Table [dbo].[Albums]    Script Date: 25/11/2020 11:27:31 ******/
+/****** Object:  Table [dbo].[Albums]    Script Date: 25/11/2020 12:49:35 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -24,4 +24,8 @@ CREATE TABLE [dbo].[Albums](
 	[AlbumID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+USE [master]
+GO
+ALTER DATABASE [Musicalog] SET  READ_WRITE 
 GO
